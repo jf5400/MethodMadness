7431f369c413ee73cce68aec66e0481f56ed95d0
BT_1N3_09 <jfreund5400@bths.edu>
2018-09-28 15:13:50 -0400

    added 2 methods

--- src/JFreundLib.java
+++ src/JFreundLib.java
@@ -1,2 +1,19 @@
 public class JFreundLib {
+    public class TestFiles {
+
+        public static Boolean isPalindrome(String word){
+            String rev="";
+            for(int i=word.length(); i>-1; i--){
+                rev=rev+word.substring(i,i+1);
+            }
+        }
+        public static String stringUnion(String word1, String word2, String word3){
+            int len1=word1.length();
+            int len2=word2.length();
+            int len3=word3.length();
+
+        }
+
+
+    }
 }
