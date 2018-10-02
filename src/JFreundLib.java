06adb1601e4d74b5b8f5bb911ebe534a181426d4
BT_1N3_15 <jfreund5400@bths.edu>
2018-10-02 14:43:20 -0400

    added files

--- src/JFreundLib.java
+++ src/JFreundLib.java
@@ -5,12 +5,7 @@ public class JFreundLib {
             for(int i=word.length(); i>0; i--){
                 rev=rev+word.substring(i-1,i);
             }
-            if(word.equals(rev)){
-                return true;
-            }
-            else{
-                return false;
-            }
+            return (word.equals(rev));
         }
         public static String stringUnion(String word1, String word2, String word3){
             int len1=word1.length();
